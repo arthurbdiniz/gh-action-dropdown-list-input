ec24a34bc6caa3b7eabdd9383908520311913306
Arthur Diniz <arthurbdiniz@gmail.com>
2021-10-24 20:30:12 -0300

    Add retry login to query input list
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- content.js
+++ content.js
@@ -1,46 +1,60 @@
-function sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
+function formatInputList(inputs) {
+    for (let input of inputs) {
+        if (input.id.startsWith("input_") && input.value.includes("[") && input.value.includes("]")) {
+            var values = input.value.split(",")
+
+            var selector = document.createElement('select');
+            selector.name = input.id
+            selector.classList = ["form-control form-select" ]
+            values.forEach(function (option, index) {
+                if (option.startsWith("[") && option.endsWith("]")) {
+                    option = option.replace("[", "").replace("]", "")
+                    optionHtml = `<option value="${option}" selected="selected">${option}</option>`
+                }else{
+                    optionHtml = `<option value="${option}">${option}</option>`
+                }
+                selector.insertAdjacentHTML('beforeend', optionHtml);
+            });
+
+            var parent = input.parentElement
+
+            if (parent.getElementsByTagName("select").length == 0) {
+                parent.appendChild(selector);
+                input.type = "hidden"
+            }
+
+            selector.onchange = function() {
+                var optionText = this.options[this.selectedIndex].text;
+                var input = document.getElementById(this.name);
+
+                input.value = input.value.replace("[", "").replace("]", "")
+                input.value = input.value.replace(optionText, `[${optionText}]`)
+            }
+        }
+    }
+}
+
+function queryInputList() {
+    var inputs = document.getElementsByTagName("input");
+    let count = 0
+    let actionInputs = []
+
+    for (let input of inputs) {
+        if (input.id.startsWith("input_") && input.value.includes("[") && input.value.includes("]")) {
+            actionInputs.push(input)
+            count++
+        }
+    }
+    if (count == 0) {
+        window.setTimeout(queryInputList, 250);
+        return;
+    }else {
+        formatInputList(actionInputs)
+    }
 }
 
 window.onclick = e => {
     if (e.target.tagName === "SUMMARY") {
-        sleep(1000).then(() => {
-            var inputs = document.getElementsByTagName("input");
-            var i = inputs.length;
-
-            while (i--) {
-                if (inputs[i].id.startsWith("input_") && inputs[i].value.includes("[") && inputs[i].value.includes("]")) {
-                    var values = inputs[i].value.split(",")
-
-                    var selector = document.createElement('select');
-                    selector.name = inputs[i].id
-                    selector.classList = ["form-control form-select" ]
-                    values.forEach(function (option, index) {
-                        if (option.startsWith("[") && option.endsWith("]")) {
-                            option = option.replace("[", "").replace("]", "")
-                            optionHtml = `<option value="${option}" selected="selected">${option}</option>`
-                        }else{
-                            optionHtml = `<option value="${option}">${option}</option>`
-                        }
-                        selector.insertAdjacentHTML('beforeend', optionHtml);
-                    });
-
-                    var parent = inputs[i].parentElement
-
-                    if (parent.getElementsByTagName("select").length == 0) {
-                        parent.appendChild(selector);
-                        inputs[i].type = "hidden"
-                    }
-
-                    selector.onchange = function() {
-                        var optionText = this.options[this.selectedIndex].text;
-                        var input = document.getElementById(this.name);
-
-                        input.value = input.value.replace("[", "").replace("]", "")
-                        input.value = input.value.replace(optionText, `[${optionText}]`)
-                    }
-                }
-            }
-        });
+        queryInputList()
     }
 }
